e45e6db0f4e4feb3bbf400b2cc3484b0028009cd
JaimeRoberts01 <0806849r@student.gla.ac.uk>
2018-02-27 22:32:41 +0000

    We can try for nothing

---
+++ findafountain/forms.py
@@ -0,0 +1,40 @@
+#Forms isn't needed but helps with tidyness, could otherwise put in models.py
+#1.) Create a model form class for each model to be rep as form
+#2.) Customize forms
+#3.) Create/update view to handle form
+	#need to display form
+	#need to save form data
+	#need to flag errors if user input wrong info
+#4.) Create/update template to display form
+#5.) Add urlpatter to map to new view
+
+#1.) ModelForm-helper class to create form from pre-existing model
+from django import forms
+from django.contrib.auth.models import User
+from findafountain.models import UserProfile
+
+
+class UserForm(forms.ModelForm):
+	password = forms.CharField(label='', widget=forms.PasswordInput(attrs={'size':40, 'placeholder':'Password'}))
+
+	class Meta:
+		model = User
+		fields =('username', 'email')
+		help_texts = {
+            'username': gettext('Some useful help text.'),
+
+        }
+        def fields_for_model(help_texts=None)
+		#labels={'username':'', 'email':'',}
+		widgets ={
+			'username': forms.TextInput(attrs={'size':40, 'placeholder':'Username'}),
+			'email': forms.TextInput(attrs={'size':40,'placeholder':'Email'}),
+		}
+		help_text=None
+
+
+class UserProfileForm(forms.ModelForm):
+	class Meta:
+		model = UserProfile
+		#fields = ('website', 'picture')
+		fields = ('picture',)
